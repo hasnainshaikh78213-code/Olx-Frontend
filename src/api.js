10d611a75793c5e62afd6c6d76154d3249befe83
Hasnain Shaikh <hasnainshaikh78213@gmail.com>
2025-10-25 10:57:50 -0700

    update

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import dotenv from "dotenv"
-dotenv.config()
+// import dotenv from "dotenv"
+// dotenv.config()
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.REACT_APP_API_URL || "http://localhost:5000/api",
 });
 
 //  automatically token 
